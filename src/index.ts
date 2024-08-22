2d6c3f635e41bad107b515c78d9f8b00d796598a
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-08-22 17:33:07 +0800

    refactor: update deps

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,9 @@
-import type { ExtensionContext } from 'vscode'
-import { createBottomBar, createSelect, registerCommand, useTheme } from '@vscode-use/utils'
+import { createBottomBar, createExtension, createSelect, registerCommand, useTheme } from '@vscode-use/utils'
 
-export function activate(context: ExtensionContext) {
+export = createExtension(() => {
   const { getAllTheme, getCurrentTheme, setTheme } = useTheme()
   const themes = getAllTheme()
-  context.subscriptions.push(registerCommand('fast-switch-theme.pick', () => {
-    const currentTheme = getCurrentTheme()
-    const options = themes.map(item => item.label)
-    const i = options.indexOf(currentTheme)
-    options.splice(i, 1)
-    options.unshift(currentTheme)
-    createSelect(options, {
-      onDidSelectItem(v: string) {
-        if (v) {
-          const id = themes.find(theme => theme.label === v)!.id || v
-          setTheme(id)
-        }
-      },
-      ignoreFocusOut: true,
-    }).then((v) => {
-      if (!v) {
-        setTheme(currentTheme)
-      }
-      else {
-        const id = themes.find(theme => theme.label === v)!.id || v
-        setTheme(id)
-      }
-    }, () => {
-      setTheme(currentTheme)
-    })
-  }))
+
   const btn = createBottomBar({
     position: 'right',
     offset: 80,
@@ -37,8 +11,32 @@ export function activate(context: ExtensionContext) {
     command: 'fast-switch-theme.pick',
   })
   btn.show()
-}
-
-export function deactivate() {
-
-}
+  return [
+    registerCommand('fast-switch-theme.pick', () => {
+      const currentTheme = getCurrentTheme()
+      const options = themes.map(item => item.label)
+      const i = options.indexOf(currentTheme)
+      options.splice(i, 1)
+      options.unshift(currentTheme)
+      createSelect(options, {
+        onDidChangeActive(v: any) {
+          if (v) {
+            const id = themes.find(theme => theme.label === v[0].label)!.id || v[0].label
+            setTheme(id)
+          }
+        },
+        ignoreFocusOut: true,
+      }).then((v: any) => {
+        if (!v) {
+          setTheme(currentTheme)
+        }
+        else {
+          const id = themes.find(theme => theme.label === v)!.id || v
+          setTheme(id)
+        }
+      }, () => {
+        setTheme(currentTheme)
+      })
+    }),
+  ]
+})
